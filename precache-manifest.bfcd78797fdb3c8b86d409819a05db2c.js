c2254f6111ee2e77a05236ffc3bde58b6f3a34c6
igor-shafran <igorshafran.personal@gmail.com>
2019-10-25 12:03:08 -0400

    Updates

---
+++ precache-manifest.bfcd78797fdb3c8b86d409819a05db2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b16ab42bf3eb6ba5d462efabdffe9e",
+    "url": "/igor-shafran/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a3d057826e712837668d",
+    "url": "/igor-shafran/monster-rolodex/static/css/main.9eefaa22.chunk.css"
+  },
+  {
+    "revision": "193f5db9d2ba56bbd4fe",
+    "url": "/igor-shafran/monster-rolodex/static/js/2.c10ccd50.chunk.js"
+  },
+  {
+    "revision": "a3d057826e712837668d",
+    "url": "/igor-shafran/monster-rolodex/static/js/main.354e6f98.chunk.js"
+  },
+  {
+    "revision": "06b365ff161cfbbe265a",
+    "url": "/igor-shafran/monster-rolodex/static/js/runtime-main.da537e68.js"
+  }
+]);
